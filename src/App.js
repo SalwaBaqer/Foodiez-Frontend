3945edc205c6ae833cecdc3145b0bc77bbea530c
Rashed Altarmoom <rashed.al6armoom@gmail.com>
2022-06-12 00:40:09 +0300

    signup done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import "./App.css";
 import CategoryList from "./components/category/CategoryList";
 import SignupModal from "./components/signup/SingupModal";
+import authStore from "./stores/authStore";
+import { observer } from "mobx-react";
 
 //components
 
 function App() {
   return (
     <div>
-      <h1>Welcome to our Foodiez Project!</h1>
+      <h1>Welcome {authStore.user.username}</h1>
       <CategoryList />
       <SignupModal />
     </div>
   );
 }
 
-export default App;
+export default observer(App);
