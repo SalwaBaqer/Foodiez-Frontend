2a643b9cc89e8b86d08a6e259aaa5a5b5cef7776
SalwaBaqer <s.abaqer@hotmail.com>
2022-06-12 00:22:34 +0300

    Add create new category components and styles

--- src/components/category/CategoryList.js
+++ src/components/category/CategoryList.js
@@ -1,16 +1,27 @@
+import React, { useState } from "react";
 import { observer } from "mobx-react";
 import categoryStore from "../../stores/categoryStore";
 //components
 import AddButton from "../buttons/AddButton";
 import CategoryItem from "./CategoryItem";
+import CreateCategoryModal from "../modals/CreateCategoryModal"
 
 function CategoryList() {
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+  function closeModal() {
+    setIsOpen(false);
+  }
   const categories = categoryStore.categories.map((category) => (
     <CategoryItem category={category} key={category._id} />
   ));
   return (
     <div>
-      <AddButton />
+      <AddButton openModal={openModal} />
+      <CreateCategoryModal modalIsOpen={modalIsOpen} closeModal={closeModal} />
       <div className="category-list-container">{categories}</div>
     </div>
   );
