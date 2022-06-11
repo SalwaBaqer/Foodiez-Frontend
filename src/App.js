4ddf20a1742173eec62733ed3d2cd253670fd15d
Rashed Altarmoom <rashed.al6armoom@gmail.com>
2022-06-12 02:02:40 +0300

    updated signup button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import CategoryList from "./components/category/CategoryList";
 import SignupModal from "./components/signup/SingupModal";
-import authStore from "./stores/authStore";
+//import authStore from "./stores/authStore";
 import { observer } from "mobx-react";
 
 //components
