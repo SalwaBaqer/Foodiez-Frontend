7f69743ccf9c9b95f3d4aae66c26e9726eae2dba
Rashed Altarmoom <rashed.al6armoom@gmail.com>
2022-06-12 17:06:10 +0300

    fixed buttons

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import CategoryList from "./components/category/CategoryList";
 import SignupModal from "./components/signup/SingupModal";
 //import authStore from "./stores/authStore";
 import { observer } from "mobx-react";
+import authStore from "./stores/authStore";
 
 //components
 
@@ -10,6 +11,7 @@ function App() {
   return (
     <div style={{ textAlign: "center" }}>
       <h1 className="title">Foodiez</h1>
+      <h2>Welcome {authStore.user.username}</h2>
       <CategoryList />
       <SignupModal />
     </div>
